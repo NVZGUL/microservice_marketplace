667d01af1709c3f50dab6362da68dfc677d02af7
NVZGUL <h3rmess@gmail.com>
2018-01-15 16:29:40 +0300

    added postgres query builder

--- services/details-catalog/utils/pg.js
+++ services/details-catalog/utils/pg.js
@@ -2,33 +2,99 @@ const { Pool } = require('pg');
 
 const cs = process.env.NODE_ENV === 'test' ? process.env.DB_CONNECT_TEST : process.env.DB_CONNECT_DEV;
 
-const pool = new Pool({
-  connectionString: cs,
-});
 
-// pool.query('CREATE TABLE ')
+const pgDb = (tableName) => {
+  const pool = new Pool({
+    connectionString: cs,
+  });
 
-const pgDb = {
-  get: async (tableName) => {
-    const res = await pool.query(`SELECT * FROM ${tableName}`);
-    await pool.end();
-    return res;
-  },
-
-  insert: async (data, tableName) => {
+  /*
+  const getShema = async () => {
     const query = `
-      INSERT INTO ${tableName}(user_id,username,password) 
-      VALUES(${data.values}) 
-      ON CONFLICT (user_id) DO UPDATE 
-      SET username = excluded.username,
-          password = excluded.password
-      RETURNING *
+      SELECT column_name, data_type FROM INFORMATION_SHEMA.COLUMNS
+      WHERE TABLE_NAME=${tableName}
     `;
-    const res = await pool.query(query);
-    await pool.end();
-    return res;
-  },
-};
+    try {
+      return await pool.query(query);
+    } catch (error) {
+      error.status = 400;
+      throw error;
+    } finally {
+      await pool.end();
+    }
+  };
+  */
+  // const columns = getShema().rows[0];
 
+  return {
+    test: async () => {
+      try {
+        return await pool.query('SELECT NOW()');
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    // get all data from the table
+    getAll: async () => {
+      try {
+        return await pool.query(`SELECT * FROM ${tableName}`);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    getDetail: async (id) => {
+      try {
+        return await pool.query(`SELECT * FROM ${tableName} WHERE detail_id=${id}`);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    insert: async (data) => {
+      const query = `
+        INSERT INTO ${tableName}(user_id,username,password) 
+        VALUES(${data.values}) 
+        ON CONFLICT (user_id) DO UPDATE 
+        SET username = excluded.username,
+            password = excluded.password
+      `;
+      try {
+        return await pool.query(query);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    search: async (param) => {
+      const query = `
+        SELECT * FROM ${tableName}
+        WHERE (COLUMN1 LIKE '%${param}%)')
+        OR (COLUMN2 LIKE '%${param}%')
+      `;
+      try {
+        return await pool.query(query);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+  };
+};
 
 module.exports = pgDb;
