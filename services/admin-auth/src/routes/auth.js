07902298b62f4b687a79434da420afafb88528ab
NVZGUL <h3rmess@gmail.com>
2018-01-16 13:20:46 +0300

    now error handle by express and removed request payload check now it's on midleware

--- services/admin-auth/src/routes/auth.js
+++ services/admin-auth/src/routes/auth.js
@@ -12,29 +12,29 @@ router.get('/getUser', authHelpers.ensureAuth, (req, res) => {
   return req.pay.success? res.status(200).json({status: "User Exist"}) : res.status(404).json({status: "User not found"})
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', async (req, res, next) => {
   const username = req.body.username;
   const user = await redisDb.findUser(username);
   if (!user.success) {
-    throw new Error(user.msg)
+    const err = new Error(user.msg);
+    err.status = 404;
+    return next(err);
   }
   let obj = user.msg
   obj.code = Math.floor(Math.random() * 90000) + 10000
   const updatedVal = await redisDb.updateValue(username, obj)
   if (!updatedVal.success) {
-    throw new Error(updatedVal.msg)
+    const err = new Error(updatedVal.msg);
+    return next(err);
   }
   twilio.messages.create({
     to: user.msg.phone,
     from: process.env.PHONE_FROM,
     body: obj.code
-  }, (err, message) => err ? res.status(500).json({ error: err }) : res.status(200).json(user))
+  }, (err, message) => err ? next(err) : res.status(200).json(user))
 })
 
-router.post('/verify', authHelpers.ensureAuth, async (req, res) => {
-  if (!req.pay.success) {
-    return res.status(404).json({msg: 'not found'})
-  }
+router.post('/verify', authHelpers.ensureAuth, async (req, res, next) => {
   return req.body.code !== req.pay.msg.code ? 
     res.status(400).json({
       msg: "code not valid"
