b78433ef31553b3d7b2dd6e386e70e7a34584af5
NVZGUL <h3rmess@gmail.com>
2018-01-16 13:21:28 +0300

    added next to correct work express error handling

--- services/admin-auth/src/app.js
+++ services/admin-auth/src/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 /* eslint-disable no-unused-vars */
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const message = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.json({
