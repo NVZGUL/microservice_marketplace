8edd8ec695e1d1fd608f266f6e282bec2a6ef025
NVZGUL <h3rmess@gmail.com>
2018-01-18 15:49:26 +0300

    added create, drop, insert queries

--- services/details-catalog/utils/pg.js
+++ services/details-catalog/utils/pg.js
@@ -2,73 +2,40 @@ const { Pool } = require('pg');
 
 const cs = process.env.NODE_ENV === 'test' ? process.env.DB_CONNECT_TEST : process.env.DB_CONNECT_DEV;
 
+const switchColumns = (col) => {
+  switch (col) {
+    case 'Код':
+      return 'code';
+    case 'Кат.номер':
+      return 'category';
+    case 'Наименование':
+      return 'description';
+    case 'Цена':
+      return 'price';
+    default:
+      return col;
+  }
+};
 
-const pgDb = (tableName) => {
+/**
+ * ORM
+ * @constructor
+ * @param {string} table - Table name.
+ */
+const pgDb = (table) => {
   const pool = new Pool({
     connectionString: cs,
   });
-
-  /*
-  const getShema = async () => {
-    const query = `
-      SELECT column_name, data_type FROM INFORMATION_SHEMA.COLUMNS
-      WHERE TABLE_NAME=${tableName}
-    `;
-    try {
-      return await pool.query(query);
-    } catch (error) {
-      error.status = 400;
-      throw error;
-    } finally {
-      await pool.end();
-    }
-  };
-  */
-  // const columns = getShema().rows[0];
-
   return {
     test: async () => {
-      try {
-        return await pool.query('SELECT NOW()');
-      } catch (error) {
-        error.status = 400;
-        throw error;
-      } finally {
-        await pool.end();
-      }
-    },
-
-    // get all data from the table
-    getAll: async () => {
-      try {
-        return await pool.query(`SELECT * FROM ${tableName}`);
-      } catch (error) {
-        error.status = 400;
-        throw error;
-      } finally {
-        await pool.end();
-      }
-    },
-
-    getDetail: async (id) => {
-      try {
-        return await pool.query(`SELECT * FROM ${tableName} WHERE detail_id=${id}`);
-      } catch (error) {
-        error.status = 400;
-        throw error;
-      } finally {
-        await pool.end();
-      }
-    },
-
-    insert: async (data) => {
       const query = `
-        INSERT INTO ${tableName}(user_id,username,password) 
-        VALUES(${data.values}) 
-        ON CONFLICT (user_id) DO UPDATE 
-        SET username = excluded.username,
-            password = excluded.password
-      `;
+      CREATE TABLE IF NOT EXISTS ${table}(
+        code integer PRIMARY KEY,
+        category varchar(256) NOT NULL,
+        description varchar(256),
+        price float(6) NOT NULL
+      )
+    `;
       try {
         return await pool.query(query);
       } catch (error) {
@@ -79,9 +46,86 @@ const pgDb = (tableName) => {
       }
     },
 
+    drop: async () => {
+      try {
+        return await pool.query(`DROP TABLE ${table}`);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+    // get all data from the table
+    getAll: async () => {
+      try {
+        return await pool.query(`SELECT * FROM ${table}`);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    getDetail: async (id) => {
+      try {
+        return await pool.query(`SELECT * FROM ${table} WHERE detail_id=${id}`);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+    /**
+     * Insert and update on conflict
+     * @constructor
+     * @param {Object} data - Data must contains two fields arr - insert array, header - columns
+     */
+    insertTest: async (data) => {
+      const arr = data.arr.reduce((a, b) => a.concat(b));
+      /* eslint-disable */
+      const values = data.arr
+        .map((a, id) => a.map((el, i) => `$${1 + i + id * a.length}`))
+        .map(l => `(${l.reduce((a, b) => a + ',' + b)})`)
+        .reduce((a, b) => `${a},${b}`);
+      /* elsint-enable */
+      const columns = data.header
+        .map(switchColumns)
+        .reduce((a, b) => a + ',' + b);
+      const query = `
+        INSERT INTO ${table}(${columns})
+        VALUES ${values}
+        ON CONFLICT (code) DO UPDATE 
+        SET category = excluded.category,
+            description = excluded.description,
+            price = excluded.price
+      `;
+      try {
+        return await pool.query(query, arr);
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
+    select: async () => {
+      try {
+        return await pool.query('SELECT * FROM test');
+      } catch (error) {
+        error.status = 400;
+        throw error;
+      } finally {
+        await pool.end();
+      }
+    },
+
     search: async (param) => {
       const query = `
-        SELECT * FROM ${tableName}
+        SELECT * FROM ${table}
         WHERE (COLUMN1 LIKE '%${param}%)')
         OR (COLUMN2 LIKE '%${param}%')
       `;
