f73bdf4dc82fff2625fa5c1488465a2d624fa030
NVZGUL <h3rmess@gmail.com>
2018-01-18 15:48:12 +0300

    added tests to post on details routes and updated created and dropping bd on start and end

--- services/details-catalog/tests/integration/routes.index.test.js
+++ services/details-catalog/tests/integration/routes.index.test.js
@@ -1,8 +1,6 @@
-process.env.NODE_ENV = 'test';
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const fs = require('fs');
+const pg = require('../../utils/pg');
 
 const should = chai.should();
 
@@ -10,33 +8,63 @@ const server = require('../../src/app');
 
 chai.use(chaiHttp);
 
-describe('GET /does/not/exist', () => {
-  it('should throw an error', (done) => {
-    chai.request(server)
-      .get('/does/not/exist')
-      .end((err, res) => {
-        should.exist(err);
-        res.status.should.equal(404);
-        res.type.should.equal('application/json');
-        done();
-      });
+describe('Details Api routes', () => {
+  beforeEach(async () => {
+    await pg('details').test();
+  });
+  afterEach(async () => {
+    await pg('details').drop();
+  });
+
+  describe('GET /does/not/exist', () => {
+    it('should throw an error', (done) => {
+      chai.request(server)
+        .get('/does/not/exist')
+        .end((err, res) => {
+          should.exist(err);
+          res.status.should.equal(404);
+          res.type.should.equal('application/json');
+          done();
+        });
+    });
+  });
+
+  describe('POST /details', () => {
+    it('should throw a file not selected error', (done) => {
+      chai.request(server)
+        .post('/details')
+        .set('authorization', 'Bearer test')
+        .end((err, res) => {
+          should.exist(err);
+          res.status.should.equal(400);
+          res.type.should.equal('application/json');
+          done();
+        });
+    });
+    it('should throw an unauthorized error', (done) => {
+      chai.request(server)
+        .post('/details')
+        .end((err, res) => {
+          should.exist(err);
+          res.status.should.equal(401);
+          res.type.should.equal('application/json');
+          done();
+        });
+    });
+    it('should post data from file', () => {
+      chai.request(server)
+        .post('/details')
+        .set('authorization', 'Bearer test')
+        .attach('myFile', 'test.xls', 'test.xls')
+        .end((err, res) => {
+          res.type.should.equal('application/json');
+          res.should.have.status(201);
+          res.body.msg.should.equal('File uploaded');
+        });
+    });
   });
 });
-
-describe('POST /details', () => {
-  it('should throw an error', (done) => {
-    chai.request(server)
-      .post('/details')
-      .field('fileField', fs.readFileSync('test.xls'))
-      .end((err, res) => {
-        should.exist(err);
-        res.status.should.equal(401);
-        res.type.should.equal('application/json');
-        done();
-      });
-  });
-});
-
+/*
 describe('GET /details/search?q=param', () => {
   it('should get all data which contains param', (done) => {
     chai.request(server)
@@ -72,3 +100,4 @@ describe('GET /details/:id', () => {
       });
   });
 });
+*/
